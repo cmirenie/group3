d011289c534cf26cc2bc71257dee251cb19a6d81
NovaMindLL <shift.kirill@mail.ru>
2025-11-06 21:31:48 +0300

    Update DataApp.java

--- DataApp.java
+++ DataApp.java
@@ -1,11 +1,14 @@
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
 public class DataApp {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Data> dataList = new ArrayList<>();
@@ -80,10 +83,29 @@ public class DataApp {
                 System.out.println("Неверный выбор.");
                 break;
         }
-
+        sortEvenNumbers(dataList);
         System.out.println("Сохраненные данные:");
         for (Data data : dataList) {
             System.out.println(data);
+        }
+
+    }
+
+    public static void sortEvenNumbers(List<Data> list) {
+        List<Integer> evenIndices = new ArrayList<>();
+        List<Data> evenValues = new ArrayList<>();
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getNumber() % 2 == 0) {
+                evenIndices.add(i);
+                evenValues.add(list.get(i));
+            }
+        }
+
+        evenValues.sort(Comparator.comparingInt(Data::getNumber));
+
+        for (int i = 0; i < evenIndices.size(); i++) {
+            list.set(evenIndices.get(i), evenValues.get(i));
         }
     }
 }
