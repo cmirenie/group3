d921dd82e4f52f7e79f9e3f57ae7999757983585
cmirenie <137637197+cmirenie@users.noreply.github.com>
2025-11-10 19:31:15 +0300

    рефакторинг SortBySpecialStrategy

--- collectionComandExecutor/commands/SortBySpecialStrategy.java
+++ collectionComandExecutor/commands/SortBySpecialStrategy.java
@@ -13,39 +13,22 @@ import java.util.List;
  * быть отсортированы в натуральном порядке, а с нечетными – оставаться на исходных позициях.
  */
 public class SortBySpecialStrategy implements CollectionStrategy {
-//    public void SortBySpecialStrategy(List<Data> list) {
-//        Data.bubbleSort(list, (d1, d2) -> {
-//            boolean d1Even = d1.getNumber() % 2 == 0;
-//            boolean d2Even = d2.getNumber() % 2 == 0;
-//
-//            if (d1Even && d2Even) {
-//                return Integer.compare(d1.getNumber(), d2.getNumber());
-//            } else if (!d1Even && !d2Even) {
-//                return 0;
-//            } else if (d1Even) {
-//                return -1;
-//            } else {
-//                return 1;
-//            }
-//        });
-//    }
 
     @Override
     public void execute(List<Data> input) {
-        List<Integer> evenIndices = new ArrayList<>();
-        List<Data> evenValues = new ArrayList<>();
+        Data.bubbleSort(input, (d1, d2) -> {
+            boolean d1Even = d1.getNumber() % 2 == 0;
+            boolean d2Even = d2.getNumber() % 2 == 0;
 
-        for (int i = 0; i < input.size(); i++) {
-            if (input.get(i).getNumber() % 2 == 0) {
-                evenIndices.add(i);
-                evenValues.add(input.get(i));
+            if (d1Even && d2Even) {
+                return Integer.compare(d1.getNumber(), d2.getNumber());
+            } else if (!d1Even && !d2Even) {
+                return 0;
+            } else if (d1Even) {
+                return -1;
+            } else {
+                return 1;
             }
-        }
-
-        evenValues.sort(Comparator.comparingInt(Data::getNumber));
-
-        for (int i = 0; i < evenIndices.size(); i++) {
-            input.set(evenIndices.get(i), evenValues.get(i));
-        }
+        });
     }
 }
